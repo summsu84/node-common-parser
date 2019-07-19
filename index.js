e299cd51a950503027795b5f29a1beb1b6577bbc
summsu84 <summsu84@gmail.com>
2019-07-20 00:51:05 +0900

    동네예보 및 중기 기상 정보 조회 모듈 베이스

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 'use strict';
 const KmaApiParser = require('./lib/kma-api-parser');
-
+const request = require('request');
 
 module.exports = {
-    'parsingResultOfMiddleForecastInfo': (type, body, onSuccess, onError) => {
-        KmaApiParser.parsingResultOfMiddleForecastInfo(type, body, onSuccess, onError);
+    'parsingResultOfMiddleForecastAndSpaceAndTemp': (type, body, onSuccess, onError) => {
+        KmaApiParser.parsingResultOfMiddleForecastAndSpaceAndTemp(type, body, onSuccess, onError);
     },
-    'parsingResultOfMiddleForecastISpaceNew': (type, body, onSuccess, onError) => {
-        KmaApiParser.parsingResultOfMiddleForecastISpaceNew(type, body, onSuccess, onError);
+    'parsingResultOfForecastSpaceData': (type, body, onSuccess, onError) => {
+        KmaApiParser.parsingResultOfForecastSpaceData(type, body, onSuccess, onError);
     },
+    'requestWeatherRequest' : (reqService, type, options, onSuccess, onError) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                if (onError)
+                    onError(error, null);
+            } else if (response.statusCode !== 200) {
+                console.error(body);
+            } else {
+                //reqService 0 : ForecastSpaceData
+                if(reqService === 0)
+                {
+                    KmaApiParser.parsingResultOfForecastSpaceData(type, body, onSuccess, onError);
+                }else
+                {
+                    KmaApiParser.parsingResultOfMiddleForecastAndSpaceAndTemp(type, body, onSuccess, onError);
+                }
+            }
+        });
+    }
 };
